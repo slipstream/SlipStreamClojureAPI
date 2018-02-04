248c985efe5ded11304e2a2e0749ef5d6ccfb6f9
sixsq-hudson <hudson@sixsq.com>
2018-02-04 06:59:07 +0000

    next development version

--- cimi/project.clj
+++ cimi/project.clj
@@ -1,6 +1,6 @@
-(def +version+ "3.45")
+(def +version+ "3.46-SNAPSHOT")
 
-(defproject com.sixsq.slipstream/SlipStreamClojureAPI-cimi "3.45"
+(defproject com.sixsq.slipstream/SlipStreamClojureAPI-cimi "3.46-SNAPSHOT"
 
   :description "Clojure CIMI API"
 
@@ -18,7 +18,7 @@
             ;; (leinjacker 0.4.3 is published and lein-codox update their leinjacker dependency to 0.4.3)
             [lein-shell "0.5.0"]]
 
-  :parent-project {:coords  [com.sixsq.slipstream/parent "3.45"]
+  :parent-project {:coords  [com.sixsq.slipstream/parent "3.46-SNAPSHOT"]
                    :inherit [:min-lein-version
                              :managed-dependencies
                              :repositories
