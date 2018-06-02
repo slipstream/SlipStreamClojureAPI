3bf1bc8172eb1bbaf8cc6fc2d4814955c456db43
sixsq-hudson <hudson@sixsq.com>
2018-06-02 12:38:13 +0000

    release v3.52

--- cimi/project.clj
+++ cimi/project.clj
@@ -1,6 +1,6 @@
-(def +version+ "3.52-SNAPSHOT")
+(def +version+ "3.52")
 
-(defproject com.sixsq.slipstream/SlipStreamClojureAPI-cimi "3.52-SNAPSHOT"
+(defproject com.sixsq.slipstream/SlipStreamClojureAPI-cimi "3.52"
 
   :description "Clojure CIMI API"
 
