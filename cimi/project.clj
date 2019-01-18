40774b070de99d06f9413763d331d6d9e8c79362
sixsq-hudson <hudson@sixsq.com>
2019-01-18 11:02:04 +0000

    next development version

--- cimi/project.clj
+++ cimi/project.clj
@@ -1,6 +1,6 @@
-(def +version+ "3.67")
+(def +version+ "3.68-SNAPSHOT")
 
-(defproject com.sixsq.slipstream/SlipStreamClojureAPI-cimi "3.67"
+(defproject com.sixsq.slipstream/SlipStreamClojureAPI-cimi "3.68-SNAPSHOT"
 
   :description "Clojure CIMI API"
 
